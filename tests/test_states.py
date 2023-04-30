1e5690a5e66f24406100c499b2e9608743444dd1
Alex <alex.blednov@gmail.com>
2023-04-30 12:13:23 +0500

    Add resets test

---
+++ tests/test_states.py
@@ -0,0 +1,15 @@
+from aiogram.dispatcher.storage import BaseStorage
+
+
+async def test_resets(
+        storage: BaseStorage
+) -> None:
+    await storage.set_data(data={'foo': 'bar'})
+    await storage.set_state(state='SECOND')
+
+    await storage.reset_data()
+    assert await storage.get_state() == 'SECOND'
+    await storage.set_data(data={'foo': 'bar'})
+
+    await storage.reset_state()
+    assert await storage.get_data() == {'foo': 'bar'}
