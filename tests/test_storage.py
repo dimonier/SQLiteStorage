1f6f4317e1d442eb93b157f25f4e4c188516e892
Alex <alex.blednov@gmail.com>
2023-04-30 12:19:32 +0500

    Exclude duplicate tests

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -4,21 +4,27 @@ from aiogram.dispatcher.storage import BaseStorage
 async def test_set_get(
         storage: BaseStorage
 ) -> None:
-    assert await storage.get_data(chat='1234') == {}
-    await storage.set_data(chat='1234', data={'foo': 'bar'})
-    assert await storage.get_data(chat='1234') == {'foo': 'bar'}
+    assert await storage.get_data() == {}
+    await storage.set_data(data={'foo': 'bar'})
+    assert await storage.get_data() == {'foo': 'bar'}
 
 
 async def test_reset(
         storage: BaseStorage
 ) -> None:
-    await storage.set_data(chat='1234', data={'foo': 'bar'})
-    await storage.reset_data(chat='1234')
-    assert await storage.get_data(chat='1234') == {}
+    await storage.set_data(data={'foo': 'bar'})
+    await storage.set_state(state='SECOND')
+
+    await storage.reset_data()
+    assert await storage.get_state() == 'SECOND'
+    await storage.set_data(data={'foo': 'bar'})
+
+    await storage.reset_state()
+    assert await storage.get_data() == {'foo': 'bar'}
 
 
 async def test_reset_empty(
     storage: BaseStorage
 ) -> None:
-    await storage.reset_data(chat='1234')
-    assert await storage.get_data(chat='1234') == {}
+    await storage.reset_data()
+    assert await storage.get_data() == {}
