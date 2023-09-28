5ab31b418d31c6706215d8faa840df37af7ef3d1
LehaSex <94061794+LehaSex@users.noreply.github.com>
2023-09-28 10:10:07 +0300

    Migration to aiogram 3.0

--- sqlitestorage/storage.py
+++ sqlitestorage/storage.py
@@ -1,5 +1,5 @@
 import sqlite3
-from aiogram.dispatcher.storage import BaseStorage
+from aiogram.fsm.storage.base import (BaseStorage, StorageKey)
 from typing import Any, Dict, Optional, Tuple
 import json
 import typing
@@ -14,11 +14,10 @@ class SQLiteStorage(BaseStorage):
 
     async def update_data(self,
                           *,
-                          chat: typing.Union[str, int, None] = None,
-                          user: typing.Union[str, int, None] = None,
+                          key: StorageKey,
                           data: typing.Dict[Any, Any] | None = None,
                           **kwargs: Any) -> None:
-        existing_data = await self.get_data(chat=chat, user=user)
+        existing_data = await self.get_data(key=key)
         if data:
             existing_data.update(data)
         existing_data.update(**kwargs)
@@ -28,28 +27,25 @@ class SQLiteStorage(BaseStorage):
         cursor.execute("""
             INSERT OR REPLACE INTO fsm_data (key, state, data)
             VALUES (?, COALESCE((SELECT state FROM fsm_data WHERE key = ?), '{}'), ?)
-        """, (str(chat) + ":" + str(user), str(chat) + ":" + str(user), json.dumps(existing_data)))
+        """, (str(key.chat_id) + ":" + str(key.user_id), str(key.chat_id) + ":" + str(key.user_id), json.dumps(existing_data)))
         conn.commit()
 
     async def update_bucket(self,
                             *,
-                            chat: typing.Union[str, int, None] = None,
-                            user: typing.Union[str, int, None] = None,
+                            key: StorageKey,
                             bucket: typing.Dict | None = None,
                             **kwargs):
         pass
 
     async def set_bucket(self,
                          *,
-                         chat: typing.Union[str, int, None] = None,
-                         user: typing.Union[str, int, None] = None,
+                         key: StorageKey,
                          bucket: typing.Dict | None = None) -> None:
         pass
 
     async def get_bucket(self,
                          *,
-                         chat: typing.Union[str, int, None] = None,
-                         user: typing.Union[str, int, None] = None,
+                         key: StorageKey,
                          default: typing.Optional[dict] | None = None) -> dict | None:
         pass
 
@@ -85,8 +81,7 @@ class SQLiteStorage(BaseStorage):
         pass
 
     async def set_state(self, *,
-                        chat: typing.Union[str, int, None] = None,
-                        user: typing.Union[str, int, None] = None,
+                        key: StorageKey,
                         state: typing.Optional[typing.AnyStr] = None,
                         **kwargs):
         conn = self._get_connection()
@@ -99,17 +94,16 @@ class SQLiteStorage(BaseStorage):
             INSERT OR REPLACE INTO fsm_data
             (key, state, data)
             VALUES (?, ?, COALESCE((SELECT data FROM fsm_data WHERE key = ?), '{}'))
-        """, (str(chat) + ":" + str(user), self.resolve_state(state), str(chat) + ":" + str(user)))
+        """, (str(key.chat_id) + ":" + str(key.user_id), self.resolve_state(state), str(key.chat_id) + ":" + str(key.user_id)))
         conn.commit()
 
     async def get_state(self,
                         *,
-                        chat: str | int | None = None,
-                        user: str | int | None = None,
+                        key: StorageKey,
                         default: str | None = None) -> typing.Coroutine[Any, Any, str | None]:
         conn = self._get_connection()
         cursor = conn.cursor()
-        cursor.execute("SELECT state FROM fsm_data WHERE key = ?", (str(chat) + ":" + str(user),))
+        cursor.execute("SELECT state FROM fsm_data WHERE key = ?", (str(key.chat_id) + ":" + str(key.user_id),))
         result = cursor.fetchone()
         # print('Get state')
         # print(f'chat: {chat}')
@@ -129,45 +123,56 @@ class SQLiteStorage(BaseStorage):
         return state
 
     async def set_data(self, *,
-                       chat: typing.Union[str, int, None] = None,
-                       user: typing.Union[str, int, None] = None,
+                       key: StorageKey,
                        data: typing.Dict | None = None):
         conn = self._get_connection()
         cursor = conn.cursor()
         cursor.execute("""
             INSERT OR REPLACE INTO fsm_data (key, state, data)
             VALUES (?, COALESCE((SELECT state FROM fsm_data WHERE key = ?), ''), ?)
-        """, (str(chat) + ":" + str(user), str(chat) + ":" + str(user), json.dumps(data)))
+        """, (str(key.chat_id) + ":" + str(key.user_id), str(key.chat_id) + ":" + str(key.user_id), json.dumps(data)))
         conn.commit()
 
     async def get_data(self, *,
-                       chat: typing.Union[str, int, None] = None,
-                       user: typing.Union[str, int, None] = None,
+                       key: StorageKey,
                        default: typing.Optional[typing.Dict] = None) -> typing.Dict:
         conn = self._get_connection()
         cursor = conn.cursor()
-        cursor.execute("SELECT data FROM fsm_data WHERE key = ?", (str(chat) + ":" + str(user),))
+        cursor.execute("SELECT data FROM fsm_data WHERE key = ?", (str(key.chat_id) + ":" + str(key.user_id),))
         result = cursor.fetchone()
         return json.loads(result[0]) if result else {}
 
     async def reset_data(self, *,
-                         chat: typing.Union[str, int, None] = None,
-                         user: typing.Union[str, int, None] = None):
-        await self.set_data(chat=chat, user=user, data={})
+                         key: StorageKey):
+        await self.set_data(key=key, data={})
 
     async def reset_state(self, *,
-                          chat: typing.Union[str, int, None] = None,
-                          user: typing.Union[str, int, None] = None,
+                          key: StorageKey,
                           with_data: typing.Optional[bool] = True):
 #        await self.set_state(chat=chat, user=user, state=None)
 #        if with_data:
 #            await self.set_data(chat=chat, user=user, data={})
-        self._cleanup(chat, user)
+        self._cleanup(key)
 
-    def _cleanup(self, chat, user):
+    def _cleanup(self, key):
 #        chat, user = self.resolve_address(chat=chat, user=user)
 #        if self.get_state(chat=chat, user=user) == None:
         conn = self._get_connection()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM fsm_data WHERE key = ?", (str(chat) + ":" + str(user),))
+        cursor.execute("DELETE FROM fsm_data WHERE key = ?", (str(key.chat_id) + ":" + str(key.user_id),))
         conn.commit()
+
+    @staticmethod
+    def resolve_state(value):
+        from aiogram.filters.state import State
+
+        if value is None:
+            return
+
+        if isinstance(value, str):
+            return value
+
+        if isinstance(value, State):
+            return value.state
+
+        return str(value)
